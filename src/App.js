d16b188da2d2cb15758272d06e65e01391528445
Tugce <cennettugce@gmail.com>
2024-08-13 17:44:49 -0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleClick = () => {
+        alert('Stay sane');
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to Psikiatrist Webpage</h1>
+                <button onClick={handleClick} style={{ padding: '10px 20px', fontSize: '16px' }}>
+                    Click me
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
